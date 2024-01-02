f9a4380d94697a367d3ae6772fe6dcad3ec3d72e
tomvothecoder <tomvothecoder@gmail.com>
2024-01-02 14:03:17 -0800

    Add non-74 GB plots

--- scripts/performance-benchmarks/spatial-avg/1-2-24-check-file-diffs.py
+++ scripts/performance-benchmarks/spatial-avg/1-2-24-check-file-diffs.py
@@ -15,14 +15,19 @@ paths3_glob = glob(os.path.join(paths3, "*.nc"))
 
 # %%
 # SerializationWarning: variable 'ta' has multiple fill values {1e+20, 1e+20}, decoding all values to NaN.
+# Shape: Frozen({"time": 60226, "plev": 8, "lat": 192, "lon": 288, nbnd: 2})
 ds1 = xc.open_mfdataset(
     paths1_glob, chunks={"time": "auto"}, add_bounds=["X", "Y", "T"], parallel=True
 )
+
+# Shape: Frozen({'time': 60225, 'bnds': 2, 'lev': 49, 'lat': 64, 'lon': 128})
 ds2 = xc.open_mfdataset(
     paths2_glob, chunks={"time": "auto"}, add_bounds=["X", "Y", "T"], parallel=True
 )
+
+# Shape: Frozen({'time': 59400, 'bnds': 2, 'plev': 8, 'lat': 324, 'lon': 432})
 ds3 = xc.open_mfdataset(
-    paths3_glob[0], chunks={"time": "auto"}, add_bounds=["X", "Y", "T"], parallel=True
+    paths3_glob, chunks={"time": "auto"}, add_bounds=["X", "Y", "T"], parallel=True
 )
 
 # %%
