2a1900c35c2b8fae4e75d58fb12860433c4203f0
tomvothecoder <tomvothecoder@gmail.com>
2024-03-26 10:44:04 -0700

    Update scripts

--- scripts/performance-benchmarks/1_esgf_download_datasets.py
+++ scripts/performance-benchmarks/1_esgf_download_datasets.py
@@ -1,19 +1,38 @@
-# %%
 import glob
 import os
 import shutil
-import stat
 import subprocess
 
 # Example: `/home/vo13/xCDAT/xcdat-validation/scripts/performance-benchmarks/`
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-# ROOT_DIR = "./scripts/performance-benchmarks/"
+
 # Example: `/home/vo13/xCDAT/xcdat-validation/scripts/performance-benchmarks/input-datasets/7gb/7gb.sh`
 WGET_FILEPATHS = glob.glob(os.path.join(ROOT_DIR, "**/**/*.sh"))
 
 
-# %%
+def main():
+    for path in WGET_FILEPATHS:
+        # -s flag is required to bypass ESG credential check.
+        # Related issue: https://github.com/esgf2-us/metagrid/issues/617
+        subprocess.call([path, "-s"])
+
+        filename = path.split("/")[-1]
+        filedir = path.split(filename)[0]
+
+        _move_ncfiles_to_dir(filedir)
+
+
 def _move_ncfiles_to_dir(dir: str):
+    """Move the nc files to the sub-directories by file size.
+
+    The wget scripts download the data to the root of the repo. After iteration,
+    this function moves those files to the correct sub-directory by file size.
+
+    Parameters
+    ----------
+    dir : str
+        The sub-directory for the respective iteration, by file size.
+    """
     filepaths = glob.glob(os.path.join("./", "*.nc"))
 
     for file in filepaths:
@@ -21,25 +40,4 @@ def _move_ncfiles_to_dir(dir: str):
 
 
 if __name__ == "__main__":
-    for path in WGET_FILEPATHS[0:1]:
-
-        # print(f"Running {path}")
-        # st = os.stat(path)
-        # os.chmod(path, st.st_mode | stat.S_IEXEC)
-
-        # -s flag is required to bypass ESG credential check.
-        # Related issue: https://github.com/esgf2-us/metagrid/issues/617
-        # proc = subprocess.run([path, "-s"], capture_output=True)
-        # output = proc.stdout.decode()
-        # print(output)
-
-        subprocess.call([path, "-s"])
-
-        # Example:
-        filename = path.split("/")[-1]
-        # Example:
-        file_dir = path.split(filename)[0]
-
-        _move_ncfiles_to_dir(file_dir)
-
-# %%
+    main()
